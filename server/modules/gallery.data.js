ece4d435fb1d4e3aaf7715fda9fab1968170ae81
Isaac Brist <isaacbrist@gmail.com>
2022-07-08 17:08:36 -0500

    I added a GET request in the App.jsx. I also added additional photos to the gallery.data file.

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,5 +1,8 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
+    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
+    { id: 2, path: 'images/myCat.jpg', description: 'Photo of my cat trying to get my attention while I code.', likes: 0 },
+    { id: 3, path: 'images/myKiddo1.jpg', description: 'Photo of my first kiddo smiling and standing in a tree, he loves to take pictures with trees.', likes: 0 },
+    { id: 4, path: 'images/myKiddo2.jpg', description: 'Photo of my second kiddo while playing with trains, he has great facial expressions!', likes: 0 }
 ];
 
 module.exports = galleryItems;
