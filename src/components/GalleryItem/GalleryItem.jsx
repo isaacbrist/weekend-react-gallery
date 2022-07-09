c87592e319e4ad0cf7209fe8b54350ba5ac185c6
Isaac Brist <isaacbrist@gmail.com>
2022-07-09 14:06:45 -0500

    I added a GalleryItem component with some styling for the images.

--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -1,25 +1,33 @@
 import { useState } from 'react';
-
-function GalleryItem({ item}) {
-
+import './GalleryItem.css';
+function GalleryItem({item}) {
+    console.log('In GalleryItem', item)
 
 
   return (
-    <tr key={item.id}>
-      
-      <td>{item}</td>
-      {/* {!itemToggleClick  ?
-        <td className="imageItem">{item.img}</td> : */}
-        <td> {item.description}</td> 
+    <>
+     <h5 key={item.id} >
+        <img src={item.path} className='imageBox'/>
+        <div> likes: {item.likes}
+     </div>
+     </h5>
+    
+    </>)
+//     <tr key={item.id}>
+// {item.description}
+//       <td>{item}</td>
+//       {/* {!itemToggleClick  ?
+//         <td className="imageItem">{item.img}</td> : */}
+//         <td> {item.description}</td> 
       
 
-      <td>
-        <button onClick={() => increaseLikesBtn(item, 'increase')}>Love it!</button>
+//       <td>
+//         <button onClick={() => increaseLikesBtn(item, 'increase')}>Love it!</button>
         
-      </td>
-    </tr>
-  );
+//       </td>
+//     </tr>
+//   );
 
-}
+ }
 
 export default GalleryItem;
