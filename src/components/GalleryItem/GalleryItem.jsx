dbe1de847b15084009e4359bc7367b39ac707059
Isaac Brist <isaacbrist@gmail.com>
2022-07-09 14:50:51 -0500

    Added conditional rendering to GalleryItem. It switches between the photo and the description. I also added some styling to the GalleryItem.css

--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -2,19 +2,30 @@ import { useState } from 'react';
 import './GalleryItem.css';
 function GalleryItem({item}) {
     console.log('In GalleryItem', item)
-
+    const [toggle, setToggle] = useState(true);
+const itemToggleClick=()=>{
+console.log('You clicked the image!')
+setToggle(!toggle)
+console.log('here is the status of toggle', toggle)
+}
 
   return (
     <>
-     <h5 key={item.id} >
-        <img src={item.path} className='imageBox'/>
+     <h5 key={item.id} onClick={itemToggleClick}>
+        <section>
+        {toggle ?
+        <p><img src={item.path}  className='imageBox' 
+        /></p>:
+        <p  className='imageBox'>{item.description} </p>
+}
+</section>
         <div> likes: {item.likes}
      </div>
      </h5>
     
     </>)
 //     <tr key={item.id}>
-// {item.description}
+// 
 //       <td>{item}</td>
 //       {/* {!itemToggleClick  ?
 //         <td className="imageItem">{item.img}</td> : */}
